345da4fe125b807631058caaa9b15f9564e344a2
estizeiger <estizeiger@gmail.com>
2021-09-29 17:27:46 +0300

    moved html to views- open all basic views

---
+++ projectDesign/projectDesign/Controllers/ConsumerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projectDesign.Controllers
+{
+    public class ConsumerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
