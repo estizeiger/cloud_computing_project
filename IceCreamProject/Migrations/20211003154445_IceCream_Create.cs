48b98e1e89c5afe405b8eb971d5a45428766708a
efiluria <eluria@g.jct.ac.il>
2021-10-04 14:01:23 +0300

    tamar: imagga basics

---
+++ IceCreamProject/Migrations/20211003154445_IceCream_Create.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IceCreamProject.Migrations
+{
+    public partial class IceCream_Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IceCream",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Flavor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Temperature = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgLocation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IceCream", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IceCream");
+        }
+    }
+}
---
+++ IceCreamProject/Models/IceCream.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamProject.Models
+{
+    public class IceCream
+    {
+        public int Id { get; set; }
+        public string Flavor { get; set; }
+
+        public double Price { get; set; }
+
+        public string Temperature { get; set; }
+        public string Description { get; set; }
+        public string  ImgLocation { get; set; }
+    }
+}
